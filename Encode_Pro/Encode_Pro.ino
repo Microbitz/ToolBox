b67ef587059523496dba34160db2cb65c33ccf10
MicroBitz <Microbitz@hotmail.com>
2020-09-17 21:48:24 -0300

    Add files via upload
    Type:
    LPD3806-600BM-G5-24C

---
+++ Encode_Pro/Encode_Pro.ino
@@ -0,0 +1,59 @@
+volatile signed int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+int Absoluto;
+float Angulo;
+
+void setup() {
+  Serial.begin (9600);
+
+  pinMode(2, INPUT_PULLUP); // internal pullup input pin 2
+  pinMode(3, INPUT_PULLUP); // internalเป็น pullup input pin 3
+  //Setting up interrupt
+  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
+  attachInterrupt(0, ai0, RISING);
+  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
+  attachInterrupt(1, ai1, RISING);
+  Serial.println(Angulo);
+}
+
+void loop() {
+  // Send the value of counter
+  if ( counter != temp ) {
+    Serial.println(Angulo);
+    if (counter < 1) {
+      Absoluto = counter * -1;
+      Angulo = 360.0 - (Absoluto * 0.3);
+      if (counter < -1200) {
+        counter = 0;
+      }
+    }
+    if (counter > 1) {
+      Absoluto = counter;
+      Angulo = Absoluto * 0.3;
+      if (counter > 1200) {
+        counter = 0;
+      }
+    }
+
+    temp = counter;
+  }
+}
+
+void ai0() {
+  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
+  // Check pin 3 to determine the direction
+  if (digitalRead(3) == LOW) {
+    counter++;
+  } else {
+    counter--;
+  }
+}
+
+void ai1() {
+  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
+  // Check with pin 2 to determine the direction
+  if (digitalRead(2) == LOW) {
+    counter--;
+  } else {
+    counter++;
+  }
+}
